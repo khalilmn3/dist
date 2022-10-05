fd65c91a3601953aef6c84612c812103ff1d2f12
khalilmn3 <khalil.mn3@gmail.com>
2022-10-05 22:37:18 +0800

    up

---
+++ dist/_nuxt/static/1664980485/en/blog/Nieuwe loyalty programmas op Blockchain/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664980485",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fblog\u002FNieuwe%20loyalty%20programmas%20op%20Blockchain",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
